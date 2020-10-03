110c2adc8a11ecba3f28f0f7f2706c00e1233375
Your Name <you@example.com>
2020-10-03 08:53:57 +0800

    模块化详解

--- 01.node/03.module.js
+++ 01.node/03.module.js
@@ -6,8 +6,21 @@
 
     使用require()引入模块以后，该函数会返回一个对象，这个对象代表的是引入的模块
 
+    我们使用require()引入外部模块时，使用的就是模块标识，我们可以通过模块标识来找到指定的模块
+        - 模块分为两大类
+            核心模块
+                - 由node引擎提供的模块
+                - 核心模块的标识就是，模块的名字
+            文件模块
+                - 由用户自己创建的模块
+                - 文件模块的标识就是文件的路径（绝对路径、相对路径）
+                    相对路径使用.或..开头
 */
 
 var md = require("./02.module");
+var math = require("./math");
+var fs = require("fs");
 
-console.log(md);
+// console.log(md);
+// console.log(math.add(123, 456));
+console.log(fs);
